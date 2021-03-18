41af0a7ea1536fc704658f221ddd287f80446999
Tom Biato <tom.biato@ynov.com>
2021-03-18 20:13:03 +0100

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { check, validationResult } from 'express-validator';
+import gravatar from 'gravatar';
+import bcrypt from 'bcryptjs';
+
+import User from '../../models/User.js';
 
 export const router = express.Router();
 
@@ -16,12 +20,47 @@ router.post(
 			'Please enter a password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty())
 			return res.status(400).json({ success: false, errors: errors.array() });
 
-		res.send('User route');
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user)
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
+
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm',
+			});
+
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			// Return JWT
+
+			res.send('User registered');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error...');
+		}
 	}
 );
